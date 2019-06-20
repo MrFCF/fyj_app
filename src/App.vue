77f90d8b7d509a24947d00e8506932f83e5b1ec1
MrFCF <fang15903057256>
2019-06-20 08:54:14 +0800

    frist commit login view

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+<script type="text/javascript">
+	export default {
+		name:'app'
+	}
+</script>
+
+<style>
+	@import "./assets/common.css";
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,154 @@
+<template>
+	<div class="login">
+		<h1>欢迎登陆保洁SFA系统</h1>
+		<div class="top_hat"></div>
+		<div class="login_box">
+			<div class="logo_warp">
+				<div class="logo_box"></div>				
+			</div>
+			<div class="input_group" :class="{active: act_index===1}">
+				<label for="cm_code">公司编号</label>
+				<input @focus="act_index=1" type="number" id="cm_code" v-model="cm_code"/>
+			</div>
+			<div class="input_group" :class="{active: act_index===2}">
+				<label for="PNO">员工编号</label>
+				<input @focus="act_index=2" type="number" id="PNO" v-model="PNO"/>
+			</div>
+			<div class="input_group" :class="{active: act_index===3}">
+				<label for="Passwd">登录密码</label>
+				<input @focus="act_index=3" type="password" id="passwd" v-model="passwd"/>
+			</div>
+			<div class="ck-row">
+				<div class="ckbox_wrap" @click="remebeSet" :class="{'active':remebe}">
+					<i class="iconfont " :class="{'icon-check-square': remebe, 'icon-border': !remebe}"></i>
+					<span>记住密码</span>
+				</div>
+				<div class="ckbox_wrap" @click="autoLoginSet" :class="{'active':auto_login}">
+					<i class="iconfont" :class="{'icon-check-square': auto_login, 'icon-border': !auto_login}"></i>
+					<span>自动登录</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	import "../assets/font/iconfont.css"
+	export default{
+		name:'login',
+		data(){
+			return{
+				act_index:1,
+				cm_code:'',
+				PNO:'',
+				passwd:'',
+				remebe:false,
+				auto_login:false
+			}
+		},
+		methods:{
+			autoLoginSet(){
+//				设置当前的auto_login为true或false
+				this.auto_login = !this.auto_login
+//				如果this.auto_login 为true 设置this.remebe 为true
+				this.auto_login && (this.remebe = true )
+			},
+			remebeSet(){
+				this.remebe = !this.remebe
+//				如果this.remebe 为false 设置this.auto_login为false
+				this.remebe || (this.auto_login = false)
+			}
+		}
+	}
+</script>
+<style type="text/css">
+	html,
+	body,
+	#app{
+		height: 100%;
+	}
+</style>
+<style lang="scss" scoped>
+	h1{
+		text-align: center;
+		font-size: px2rem(36);
+		height: px2rem(36);
+		padding: px2rem(148-36-44) 0 px2rem(44) 0;
+		line-height: px2rem(36);
+		color: #fff;
+	}
+	.login{
+		background-color: #2ade69;
+		height: 100%;
+		
+		.top_hat{
+			width: px2rem(573);
+			height: px2rem(18);
+			background-color: #eee;
+			margin: 0 auto;
+			border-radius:px2rem(18) px2rem(18) 0 0;
+		}
+		.login_box{
+			width: px2rem(600);
+			height: px2rem(836);
+			background-color: #FFFFFF;
+			border-radius: px2rem(20);
+			margin: 0 auto;
+			.logo_warp{
+				padding: px2rem(80) 0;
+				
+				.logo_box{
+					width: px2rem(190);
+					height: px2rem(190);
+					margin: 0 auto;
+					background-image: url('./../assets/logo.jpg');
+					background-size: cover;
+				}
+			}
+			@mixin rowStyle(){
+				padding: 0 px2rem(36);
+				color: $text-color;
+				width: px2rem(400);
+			}
+			.input_group{
+				border:2px solid #e2e2e2;
+				border-radius: px2rem(45);
+				font-size: px2rem(30);
+				line-height: px2rem(90);
+				@include rowStyle();
+				margin: 0 auto px2rem(30) auto;
+				
+				input{
+					border: 0 none;
+					font-size: $text-size-mid;
+					line-height: px2rem(90);
+					width: px2rem(200);
+					padding-left: px2rem(10);
+				}
+			}
+			.input_group.active{
+				color: $act-color;
+				border: 2px solid $act-color;
+			}
+			.ck-row{
+				font-size: $text-size;
+				@include rowStyle();
+				display: flex;
+				justify-content: space-around;
+				.ckbox_wrap{
+					padding-top: px2rem(8);
+					padding-left: px2rem(36);
+					i::before{
+						display: inline-block;
+						line-height:px2rem(28);
+						height: px2rem(28);
+						font-size:px2rem(28);
+					}
+				}
+			}
+			.ckbox_wrap.active{
+				color: $act-color;
+			}
+		}
+	}
+</style>
